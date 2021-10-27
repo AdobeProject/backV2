b0644c2a1584890268dc24260f49714f46c7fde7
Vardan Melkonyan <vardan.melkonyan@tumo.org>
2021-10-27 21:34:26 +0400

    Add history service

--- src/main/java/com/example/demo/repository/HistoryRepository.java
+++ src/main/java/com/example/demo/repository/HistoryRepository.java
@@ -1,9 +1,13 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.History;
+import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface HistoryRepository extends JpaRepository<History,Long> {
+	List<History> findAllByUser(User user);
 }
