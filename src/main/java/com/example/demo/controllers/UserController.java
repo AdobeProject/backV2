0a76a198f73a0ddbe7badb93c5a552420a8e66b5
Vardan Melkonyan <vardan.melkonyan@tumo.org>
2021-10-27 21:24:15 +0400

    Add default message

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
-//@CrossOrigin("*")
 public class UserController {
 
 	private final UserApiFacade userApiFacade;
